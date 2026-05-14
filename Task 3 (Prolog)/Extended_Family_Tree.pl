d0c2556c6b55e9b96659ff9629f3ba9164629e49
TobiasOmollo <237821654+TobiasOmollo@users.noreply.github.com>
2026-05-14 13:55:20 +0300

    Implement family tree facts and relationships
    Added facts and rules for an extended family tree in Prolog.

--- Task 3 (Prolog)/Extended_Family_Tree.pl
+++ Task 3 (Prolog)/Extended_Family_Tree.pl
@@ -1,1 +1,33 @@
+%A simple illustration of a Family Tree constising of One parent
+%4 Children
+%And 4 GrandChildren
+%The First Part is always about the factual information
+%Facts
+parent(ngani, salome).
+parent(ngani, roba).
+parent(ngani, rose).
+parent(ngani, kerina).
+parent(salome, viki).
+parent(roba, chief).
+parent(rose, mike).
+parent(kerina, mercy).
+
+male(ngani).
+male(roba).
+male(chief).
+male(mike).
+
+female(salome).
+female(rose).
+female(kerina).
+female(viki).
+female(mercy).
+
+
+%Rules
+%A description of how the members relate to each other
+grandparent(X, Z):- parent(X, Y) , parent(X, Y), parent(Y,Z).
+aunt(X,Z):- parent(B,X), parent(B,Y), parent(Y,Z), female(X).
+uncle(X,Z):- parent(B,X), parent(B,Y), parent(Y,Z), male(X).
+niece(X,Z):- parent(A,Y), parent(A,Z), parent(Y,X),female(X).
 
